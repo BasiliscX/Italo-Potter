b70ce0ff1ad20f588ef02d9f5b6a3612713286f4
BasiliscX <juniornavarro1988@gmail.com>
2024-07-09 15:23:46 -0300

    initial commit

--- res/resources.h
+++ res/resources.h
@@ -2,9 +2,6 @@
 #define _RES_RESOURCES_H_
 
 extern const Image bg1;
-extern const TileSet stadium_tileset;
-extern const MapDefinition stadium_map;
-extern const Palette gh_palette;
 extern const SpriteDefinition sprite;
 
 #endif // _RES_RESOURCES_H_
