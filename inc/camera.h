e4bd8bb37203816ba86464b23ccd8bd1bd274761
BasiliscX <juniornavarro1988@gmail.com>
2024-07-09 22:28:49 -0300

    functional model

---
+++ inc/camera.h
@@ -0,0 +1,84 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <genesis.h>
+#include "player.h"
+
+#define HOW_FAR_TO_LEFT_BEFORE_CAMERA_MOVES 152
+#define HOW_FAR_TO_RIGHT_BEFORE_CAMERA_MOVES 153
+#define HOW_FAR_TO_TOP_BEFORE_CAMERA_MOVES 115
+#define HOW_FAR_TO_BOTTOM_BEFORE_CAMERA_MOVES 116
+
+#define HORIZONTAL_RESOLUTION 320
+#define VERTICAL_RESOLUTION 224
+#define MAP_WIDTH 512
+#define MAP_HEIGHT 512
+
+int current_camera_x;
+int current_camera_y;
+
+void initCamera() {
+    current_camera_x = 0;
+    current_camera_y = 0;
+}
+
+void updateCamera() {
+    if(player_x < FIX32(0)){
+        player_x = FIX32(0);
+    } else if(player_x > FIX32(MAP_WIDTH - PLAYER_WIDTH)){
+        player_x = FIX32(MAP_WIDTH - PLAYER_WIDTH);
+    }
+
+    if(player_y < FIX32(0)){
+        player_y = FIX32(0);
+    } else if(player_y > FIX32(MAP_HEIGHT - PLAYER_HEIGHT)){
+        player_y = FIX32(MAP_HEIGHT - PLAYER_HEIGHT);
+    }
+
+    int player_x_map_integer = fix32ToInt(player_x);
+    int player_y_map_integer = fix32ToInt(player_y);
+
+    int player_x_position_on_screen = player_x_map_integer - current_camera_x;
+    int player_y_position_on_screen = player_y_map_integer - current_camera_y;
+
+    int new_camera_x, new_camera_y;
+
+    if(player_x_position_on_screen > HOW_FAR_TO_RIGHT_BEFORE_CAMERA_MOVES){
+        new_camera_x = player_x_map_integer - HOW_FAR_TO_RIGHT_BEFORE_CAMERA_MOVES;        
+    } else if(player_x_position_on_screen < HOW_FAR_TO_LEFT_BEFORE_CAMERA_MOVES){
+        new_camera_x = player_x_map_integer - HOW_FAR_TO_LEFT_BEFORE_CAMERA_MOVES;
+    } else{
+        new_camera_x = current_camera_x;
+    }
+
+    if(player_y_position_on_screen > HOW_FAR_TO_BOTTOM_BEFORE_CAMERA_MOVES){
+        new_camera_y = player_y_map_integer - HOW_FAR_TO_BOTTOM_BEFORE_CAMERA_MOVES;        
+    } else if(player_y_position_on_screen < HOW_FAR_TO_TOP_BEFORE_CAMERA_MOVES){
+        new_camera_y = player_y_map_integer - HOW_FAR_TO_TOP_BEFORE_CAMERA_MOVES;
+    } else{
+        new_camera_y = current_camera_y;
+    }
+
+    if(new_camera_x < 0){
+        new_camera_x = 0;
+    } else if(new_camera_x > (MAP_WIDTH - HORIZONTAL_RESOLUTION)){
+        new_camera_x = MAP_WIDTH - HORIZONTAL_RESOLUTION;
+    }
+    
+    if(new_camera_y < 0){
+        new_camera_y = 0;
+    } else if(new_camera_y > (MAP_HEIGHT - VERTICAL_RESOLUTION)){
+        new_camera_y = MAP_HEIGHT - VERTICAL_RESOLUTION;
+    }
+
+    if((current_camera_x != new_camera_x) || (current_camera_y != new_camera_y)){
+        current_camera_x = new_camera_x;
+        current_camera_y = new_camera_y;
+
+        MAP_scrollTo(level_1_map, new_camera_x, new_camera_y);
+    }
+
+    SPR_setPosition(player, fix32ToInt(player_x) - new_camera_x, fix32ToInt(player_y) - new_camera_y);
+}
+
+#endif // CAMERA_H
--- res/resources.res
+++ res/resources.res
@@ -1,16 +1,3 @@
-# IMAGE bg1 "img/zwerkbalveld-8x8-1p16c-s.bmp" BEST ALL
-
-# IMAGE bg1 "img/large-map.png" BEST ALL
-
-# FUNCIONAL:
-# IMAGE bg1 "img/zwerkbalveld-8x8-1p16c-s - copia.bmp" BEST ALL
-
-# TILESET stadium_tileset "img/large-map-8x8-1p16c-s.bmp" BEST ALL 
-# MAP stadium_map "img/large-map-8x8-1p16c-s.bmp" stadium_tileset BEST 0
-# PALETTE gh_palette "img/large-map-8x8-1p16c-s.bmp"
-
-
-
 TILESET stadium_tileset "img/large-map.png" BEST ALL 
 MAP stadium_map "img/large-map.png" stadium_tileset BEST 0
 PALETTE gh_palette "img/large-map.png"
@@ -18,5 +5,3 @@ PALETTE gh_palette "img/large-map.png"
 
 
 SPRITE sprite "img/italo-sheet.png" 6 6 FAST 4
-
-# SPRITE sprite "img/animated sprite 15 16.png" 15 16 FAST 16
